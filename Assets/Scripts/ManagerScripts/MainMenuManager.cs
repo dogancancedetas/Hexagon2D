8c015f1006abe9a8c515e832bc90116c4a29ba9a
Dogancan Cedetas <dogancancedetas@hotmail.com>
2021-12-26 15:37:36 +0300

    added: play and exit to menu buttons

---
+++ Assets/Scripts/ManagerScripts/MainMenuManager.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuManager : MonoBehaviour
+{
+    public void PlayGame()
+    {
+        SceneManager.LoadScene("Gameplay");
+    }
+    public void ExitGame()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+}
